bac73f1a9b034df24d5e249f3149e2758754ffae
Reenal-M <reenal.21ai038@sode-edu.in>
2023-05-31 15:56:20 +0530

    Update crowdfunding.sol

--- web3/contracts/crowdfunding.sol
+++ web3/contracts/crowdfunding.sol
@@ -34,19 +34,34 @@ function createCamapign(address _owner,string memory _title,string memory _descr
     numberOfCampaigns++;
 
     return numberOfCampaigns -1;
+}
 
+function donateToCampaign(uint256 _id) public payable {
+    uint256 amount = msg.value;
+
+    Campaign storage campaign = campaigns[_id];
+    campaign.donators.push(msg.sender);
+    campaign.donations.push(amount);
+
+    (bool sent,) = payable(campaign.owner).call{value: amount}("");
+
+    if(sent){
+        campaign.amountCollected = campaign.amountCollected + amount;
+    }
 
 }
 
-function donateToCamapign() {}
+function getDonators(uint256 _id)view public returns (address[] memory, uint256[] memory){
+return(campaigns[_id].donators, campaigns[_id].donations);
+}  
 
-function getDonators() {}
-
-function getCampaigns() {}
-
-
-
-
-
-
+function getCampaigns()public view returns (campaign[] memory){
+    Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
 }
+}
+
+
+
+
+
+
